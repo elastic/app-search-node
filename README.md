4e095582e2282fcb289acb0a62f06fb9c891d5e3
Marshall Scorcio <marshall.scorcio@gmail.com>
2018-02-27 14:01:34 -0800

    Initial client implementation

--- README.md
+++ README.md
@@ -1,1 +1,98 @@
 # Node client for the Swiftype App Search Api
+
+**Note: Swiftype App Search is currently in beta**
+
+## Installation
+
+To install the gem, execute:
+
+    npm install swiftype-app-search-node
+
+## Usage
+
+### Setup: Configuring the client and authentication
+
+Create a new instance of the Swiftype App Search Client. This requires your ACCOUNT_HOST_KEY, which identifies the unique hostname of the Swiftype API that is associated with your Swiftype account. It also requires a valid API_KEY, which authenticates requests to the API:
+
+    const SwiftypeAppSearchClient = require('swiftype-app-search-node')
+    const accountHostKey = 'host-c5s2mj'
+    const apiKey = 'api-mu75psc5egt9ppzuycnc2mc3'
+    const client = new SwiftypeAppSearchClient(accountHostKey, apiKey)
+
+### Indexing: Creating and updating Documents
+
+    const engineName = 'favorite-videos'
+    const documents = [
+      {
+        id: 'INscMGmhmX4',
+        url: 'https://www.youtube.com/watch?v=INscMGmhmX4',
+        title: 'The Original Grumpy Cat',
+        body: 'A wonderful video of a magnificent cat.'
+      },
+      {
+        id: 'JNDFojsd02',
+        url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        title: 'Another Grumpy Cat',
+        body: 'A great video of another cool cat.'
+      }
+    ]
+
+
+    client.indexDocuments(engineName, documents)
+    .then((indexDocumentResults) => {
+      // handle index document results
+    })
+    .catch((error) => {
+      // handle error
+    })
+
+### Retrieving Documents
+
+    engineName = 'favorite-videos'
+    documentIds = ['INscMGmhmX4', 'JNDFojsd02']
+
+    client.getDocuments(engineName, documentIds)
+    .then((documentContents) => {
+      // handle document contents
+    })
+    .catch((error) => {
+      // handle error
+    })
+
+### Destroying Documents
+
+    engineName = 'favorite-videos'
+    documentIds = ['INscMGmhmX4', 'JNDFojsd02']
+
+    client.destroyDocuments(engineName, documentIds)
+    .then((destroyDocumentResults) => {
+      // handle destroy document results
+    })
+    .catch((error) => {
+      // handle error
+    })
+
+### Searching
+
+    engineName = 'favorite-videos'
+    query = 'cat'
+    searchFields = { :title => {} }
+    resultFields = { :title => { :raw => {} } }
+    options = { search_fields: searchFields, result_fields: resultFields }
+
+    client.search(engineName, query, options)
+    .then((searchResults) => {
+      // handle search results
+    })
+    .catch((error) => {
+      // handle error
+    })
+
+
+## Running Tests
+
+    $ npm test
+
+## Contributions
+
+  To contribute code to this gem, please fork the repository and submit a pull request.
---
+++ test/test.js
@@ -0,0 +1,121 @@
+const assert = require('assert')
+const SwiftypeAppSearchClient = require('../lib/swiftypeAppSearch')
+const replay = require('replay')
+
+describe('SwiftypeAppSearchClient', () => {
+  const accountHostKey = 'host-c5s2mj'
+  const apiKey = 'api-mu75psc5egt9ppzuycnc2mc3'
+  const engineName = 'swiftype-api-example'
+  const documents = [
+    {
+      id: 'INscMGmhmX4',
+      url: 'http://www.youtube.com/watch?v=v1uyQZNg2vE',
+      title: 'The Original Grumpy Cat',
+      body: 'this is a test'
+    },
+    {
+      id: 'JNDFojsd02',
+      url: 'http://www.youtube.com/watch?v=tsdfhk2j',
+      title: 'Another Grumpy Cat',
+      body: 'this is also a test'
+    }
+  ]
+
+  const swiftype = new SwiftypeAppSearchClient(accountHostKey, apiKey)
+
+  describe('#indexDocuments', () => {
+    it('should index documents successfully', (done) => {
+      swiftype.indexDocuments(engineName, documents)
+      .then((results) => {
+        assert.deepEqual([
+          { "errors": [], "id": "INscMGmhmX4" },
+          { "errors": [], "id": "JNDFojsd02" }
+        ], results)
+        done()
+      })
+      .catch((error) => {
+        done(error)
+      })
+    })
+
+    it('should raise an error if any document does not have an id', (done) => {
+      assert.throws(() => {
+        swiftype.indexDocuments(engineName, [
+          { title: 'foo' }
+        ])
+      }, /missing required fields \(id\)/)
+      done()
+    })
+  })
+
+  describe('#getDocuments', () => {
+    const documentIds = documents.map((d) => d.id)
+
+    it('should get documents successfully', (done) => {
+      swiftype.getDocuments(engineName, documentIds)
+      .then((results) => {
+        assert.deepEqual([
+          {
+            "body": "this is a test",
+            "id": "INscMGmhmX4",
+            "title": "The Original Grumpy Cat",
+            "url": "http://www.youtube.com/watch?v=v1uyQZNg2vE",
+          },
+          {
+            "body": "this is also a test",
+            "id": "JNDFojsd02",
+            "title": "Another Grumpy Cat",
+            "url": "http://www.youtube.com/watch?v=tsdfhk2j",
+          }
+        ], results)
+        done()
+      })
+      .catch((error) => {
+        done(error)
+      })
+    })
+  })
+
+  describe('#destroyDocuments', () => {
+    it('should destroy documents', (done) => {
+      swiftype.destroyDocuments(engineName, ["INscMGmhmX4", "FakeId"])
+      .then((results) => {
+        assert.deepEqual([
+          { "id": "INscMGmhmX4", "result": true },
+          { "id": "FakeId", "result": false },
+        ], results)
+        done()
+      })
+      .catch((error) => {
+        done(error)
+      })
+    })
+  })
+
+  describe('#search', () => {
+    it('should query', (done) => {
+      swiftype.search(engineName, 'cat')
+      .then((resp) => {
+        assert.deepEqual([
+          { raw: 'The Original Grumpy Cat' },
+          { raw: 'Another Grumpy Cat' }
+        ], resp.results.map((r) => r.title ))
+        done()
+      })
+      .catch((error) => {
+        done(error)
+      })
+    })
+  })
+
+  describe('#createSignedSearchKey', () => {
+    it('should build a valid jwt', (done) => {
+      token = SwiftypeAppSearchClient.createSignedSearchKey('api-mu75psc5egt9ppzuycnc2mc3', 42, { query: 'cat' })
+      jwt = require('jsonwebtoken')
+      decoded = jwt.verify(token, 'api-mu75psc5egt9ppzuycnc2mc3')
+      assert.equal(decoded.api_key_id, 42)
+      assert.equal(decoded.query, 'cat')
+      done()
+    })
+  })
+})
