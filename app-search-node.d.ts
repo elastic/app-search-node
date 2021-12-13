7f0825962b962a853ca4d0b726ddaac3b227679a
Jason Stoltzfus <jastoltz24@gmail.com>
2021-12-13 15:24:24 -0500

    Add client

--- app-search-node.d.ts
+++ app-search-node.d.ts
@@ -705,3 +705,21 @@ declare module "@elastic/app-search-node" {
     ): void;
   }
 }
+
+declare module "@elastic/app-search-node/lib/client" {
+  export = Client;
+  declare class Client {
+    constructor(apiKey: any, baseUrl: any);
+    apiKey: string;
+    clientName: string;
+    clientVersion: string;
+    baseUrl: string;
+    get(path: string, params: any): Promise<any>;
+    post(path: string, params: any): Promise<any>;
+    put(path: string, params: any): Promise<any>;
+    patch(path: string, params: any): Promise<any>;
+    delete(path: string, params: any): Promise<any>;
+    _jsonRequest(method: any, path: string, params: any): Promise<any>;
+    _wrap(options: any): Promise<any>;
+  }
+}
